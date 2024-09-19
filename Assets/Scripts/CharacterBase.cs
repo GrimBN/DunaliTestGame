23e0ae76a0f8fdb286381ff4faf5cae1b5b46e15
Agrim Bhasin <bhasin.agrim996@gmail.com>
2024-09-20 01:15:34 +0530

    Partially implemented empty cell check + partially setup zombie animations

--- Assets/Scripts/CharacterBase.cs
+++ Assets/Scripts/CharacterBase.cs
@@ -8,7 +8,7 @@ public delegate void  MoveDelegate();
 public abstract class CharacterBase : MonoBehaviour
 {
     [SerializeField] protected int maxHealth;
-    [SerializeField] protected AnimatorController animatorController;
+    [SerializeField] protected Animator animator;
     [SerializeField] protected Grid grid;
 
     protected int currentHealth;
@@ -22,7 +22,8 @@ public abstract class CharacterBase : MonoBehaviour
         currentHealth = maxHealth;
         isAlive = true;
         grid = GetComponentInParent<Grid>();
-        Vector3Int currentGridPos = grid.WorldToCell(transform.localPosition);
+        transform.localPosition = GetLocalPosFromCellPos(Vector3Int.zero);
+        animator = GetComponent<Animator>();
 
     }
 
@@ -31,24 +32,50 @@ public abstract class CharacterBase : MonoBehaviour
         Init();
     }
 
-    public void RelativeMoveTo(Vector3Int relativeGridPosition)
+    protected void RelativeMoveTo(Vector3Int relativeGridPosition)
     {
-        Vector3Int currentGridPos = grid.LocalToCell(transform.localPosition);
-        Vector3Int targetGridPos = currentGridPos + relativeGridPosition;
-        Vector3 targetPos = new Vector3(grid.GetCellCenterLocal(targetGridPos).x, 0f, grid.GetCellCenterLocal(targetGridPos).z) ;
-
-         Debug.Log("Current grid pos:" + currentGridPos + "\nTarget grid pos:" + targetGridPos + "\nTarget Local pos:" + targetPos);
+        Vector3 targetPos = GetLocalPosFromCellPos(relativeGridPosition);
 
         StartCoroutine(MoveCoroutine(targetPos));
-        
     }
 
-    public IEnumerator MoveCoroutine(Vector3 targetPos)
+    protected Vector3 GetLocalPosFromCellPos(Vector3Int cellPosition)
+    {
+        Vector3Int currentGridPos = grid.LocalToCell(transform.localPosition);
+        Vector3Int targetGridPos = currentGridPos + cellPosition;
+        Vector3 targetPos = new Vector3(grid.GetCellCenterLocal(targetGridPos).x, 0f, grid.GetCellCenterLocal(targetGridPos).z);
+        return targetPos;
+    }
+
+    protected bool CheckTargetCellEmpty(Vector3Int cellPosition)
+    {
+        Vector3 boxCastCenter = transform.position;
+        Bounds bounds = grid.GetBoundsLocal(cellPosition);
+        Vector3 posCheck = grid.LocalToWorld(GetLocalPosFromCellPos(cellPosition));
+        Vector3 direction = (posCheck - boxCastCenter).normalized;
+
+        LayerMask layerMask = LayerMask.NameToLayer("Hurdle");
+        
+        if (Physics.BoxCast(boxCastCenter, bounds.extents, direction, out RaycastHit hit, Quaternion.identity, (posCheck - boxCastCenter).magnitude))
+        {
+            Debug.Log(hit.collider.gameObject.name);
+            return false;
+        }
+        return true;
+    }
+
+    private IEnumerator MoveCoroutine(Vector3 targetPos)
     {
         Vector3 startPos = transform.localPosition;
         float speed = Vector3.Distance(startPos, targetPos) / GameController.Instance.CharacterMoveTime;
         float timer = 0f;
-        while( timer < GameController.Instance.CharacterMoveTime && Vector3.Distance(transform.localPosition, targetPos) > 0.01f)
+
+        if (animator)
+        {
+            animator.SetBool("Moving", true);
+        }
+
+        while ( timer < GameController.Instance.CharacterMoveTime)// && Vector3.Distance(transform.localPosition, targetPos) > 0.01f)
         {
             Vector3 step = Vector3.MoveTowards(transform.localPosition, targetPos, speed * Time.deltaTime);
             transform.localPosition = step;
@@ -60,6 +87,12 @@ public abstract class CharacterBase : MonoBehaviour
         transform.localPosition = targetPos;
 
         MoveFinished?.Invoke();
+    }
+
+    protected void Turn(Vector3Int relativeGridPosition)
+    {
+        Vector3 targetPos = GetLocalPosFromCellPos(relativeGridPosition);
+        transform.LookAt(targetPos);
     }
 
     public void Attack()
--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -4,28 +4,47 @@ using UnityEngine;
 
 public class PlayerCharacter : CharacterBase
 {
-    private bool canMove;
+    private bool canAct;
     void Start()
     {
         Init();
         MoveFinished += OnMoveFinished;
-        canMove = true;
+        canAct = true;
     }
 
     
     void Update()
     {
-        if( canMove && (Mathf.Abs(Input.GetAxis("Horizontal")) > 0 || Mathf.Abs(Input.GetAxis("Vertical")) > 0))
+        if (canAct && PlayerInput())
         {
             Vector3Int direction = new Vector3Int(Mathf.RoundToInt(Input.GetAxisRaw("Horizontal")), Mathf.RoundToInt(Input.GetAxisRaw("Vertical")), 0);
-            //Vector3Int directionInt = new Vector3Int(Mathf.RoundToInt(direction.x), Mathf.RoundToInt(direction.y), 0);
-            RelativeMoveTo(direction);
-            canMove = false;
+            
+            if(CheckTargetCellEmpty(grid.LocalToCell(transform.localPosition) + direction))
+            {
+                RelativeMoveTo(direction);
+                Turn(direction);
+            }
+            canAct = false;
+
+
         }
+        else if (canAct && animator.GetBool("Moving"))
+        {
+            animator.SetBool("Moving", false);
+        }
+    }
+
+    private static bool PlayerInput()
+    {
+        return Mathf.Abs(Input.GetAxis("Horizontal")) > 0 || Mathf.Abs(Input.GetAxis("Vertical")) > 0;
     }
 
     private void OnMoveFinished()
     {
-        canMove = true;
+        canAct = true;
+        if(animator && !PlayerInput())
+        {
+            animator.SetBool("Moving", false);
+        }
     }
 }
